ed81fe97aeccd11a9c0936cce7d460bb5c59176f
Satyam Raj <singhrajsatyam17@gmail.com>
2025-06-30 06:06:09 +0530

    Add files via upload

---
+++ 6360512-week 2/1st NUnit Handson/CalculatorSolution/CalcLibrary.Tests/Calculatortest.cs
@@ -0,0 +1,30 @@
+﻿using NUnit.Framework;
+using CalcLibrary;
+
+namespace CalcLibrary.Tests
+{
+    [TestFixture] // 1. TestFixture attribute
+    public class CalculatorTests
+    {
+        private ICalculator _calculator;
+
+        [SetUp] // 2. Setup (initialize)
+        public void Setup() => _calculator = new Calculator();
+
+        [TearDown] // 3. Teardown (cleanup)
+        public void Cleanup() => _calculator = null;
+
+        // 4. Parameterized test with TestCase
+        [TestCase(5, 7, 12)]
+        [TestCase(-1, 1, 0)]
+        public void Add_ValidInputs_ReturnsSum(double a, double b, double expected)
+        {
+            var result = _calculator.Add(a, b);
+            Assert.That(result, Is.EqualTo(expected)); // 5. Assert.That
+        }
+
+        [Test]
+        [Ignore("Pending implementation")] // 6. Ignore attribute
+        public void Subtract_NotImplemented_ShouldBeIgnored() { }
+    }
+}
