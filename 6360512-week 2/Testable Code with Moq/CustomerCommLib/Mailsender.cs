001d43f6cdd9e21f1bc4ca884a3a648fd987e8bf
Satyam Raj <singhrajsatyam17@gmail.com>
2025-06-30 05:38:12 +0530

    Add files via upload

---
+++ 6360512-week 2/Testable Code with Moq/CustomerCommLib/Mailsender.cs
@@ -0,0 +1,46 @@
+﻿using System.Net;
+using System.Net.Mail;
+
+namespace CustomerCommLib
+{
+    public interface IMailSender
+    {
+        bool SendMail(string toAddress, string message);
+    }
+
+    public class MailSender : IMailSender
+    {
+        public bool SendMail(string toAddress, string message)
+        {
+            var mail = new MailMessage();
+            var smtpServer = new SmtpClient("smtp.gmail.com")
+            {
+                Port = 587,
+                Credentials = new NetworkCredential("username", "password"),
+                EnableSsl = true
+            };
+
+            mail.From = new MailAddress("your_email_address@gmail.com");
+            mail.To.Add(toAddress);
+            mail.Subject = "Test Mail";
+            mail.Body = message;
+
+            smtpServer.Send(mail);
+            return true;
+        }
+    }
+    public class CustomerComm
+    {
+        private readonly IMailSender _mailSender;
+
+        public CustomerComm(IMailSender mailSender)
+        {
+            _mailSender = mailSender ?? throw new ArgumentNullException(nameof(mailSender));
+        }
+
+        public bool SendMailToCustomer()
+        {
+            return _mailSender.SendMail("cust123@abc.com", "Some Message");
+        }
+    }
+}
---
+++ 6360512-week 2/Testable Code with Moq/CustomerTests/CustomercommTests.cs
@@ -0,0 +1,32 @@
+﻿using Moq;
+using NUnit.Framework;
+using CustomerCommLib;
+
+namespace CustomerComm.Tests
+{
+    [TestFixture]
+    public class CustomerCommTests
+    {
+        private Mock<IMailSender> _mockMailSender;
+        private CustomerComm _customerComm;
+
+        [OneTimeSetUp]
+        public void Setup()
+        {
+            _mockMailSender = new Mock<IMailSender>();
+            _mockMailSender
+                .Setup(m => m.SendMail(It.IsAny<string>(), It.IsAny<string>()))
+                .Returns(true);
+
+            _customerComm = new CustomerComm(_mockMailSender.Object);
+        }
+
+        [Test]
+        public void SendMailToCustomer_ReturnsTrue_WhenMocked()
+        {
+            var result = _customerComm.SendMailToCustomer();
+
+            Assert.IsTrue(result);
+        }
+    }
+}
